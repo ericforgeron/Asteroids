52d6a4cc8c7ae5a5848fb9cd357e77401d7a39c7
ericforgeron <eforgeron@outlook.com>
2026-03-04 09:18:55 -0800

    Chapter 3 Lesson 2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -12,12 +13,16 @@ def main():
     clock = pygame.time.Clock() #this is my screen clock object
     dt = 0 # this is my delta time variable
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #This is where I create the screen for the game
+    player  = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     while True:  # An infinite loop to keep the game going until a condition is met that breaks the loop
         log_state() # writes the state of the app to the log file
         for event in pygame.event.get(): # For loop used to check for input, update the game, redraw the game
            if event.type == pygame.QUIT:
                return
-        pygame.Surface.fill(screen, "black") # Gives the game a black background
+        screen.fill("black") # makes the background black
+        player.draw(screen) # draws my spaceship to the screen
         pygame.display.flip() # redraws the screen with effect of user input
         clock.tick(60) # this is how I run the game at 60 frames per second
         dt = clock.tick() / 1000 # saving the delta time between each loop in seconds (milliseconds/1000)
